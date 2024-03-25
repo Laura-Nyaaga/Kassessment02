f661c68b96891c94f641cf82973efbfd10dbb23c
Laura-Nyaaga <158445841+Laura-Nyaaga@users.noreply.github.com>
2024-03-25 08:52:43 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,9 @@
 fun main() {
 exclude("Barnie bakes brown bagels and buns")
     numbers(arrayOf(2,3,4))
-    volume(0.75, 3.14159, 7.0)
+    // volume(0.75, 3.14159, 7.0)
+    var sphere = Volume (3.14159, 0.75, 7.00)
+    sphere.volume()
     isPalindrome("madam")
     isPalindrome("laura")
 }
@@ -12,9 +14,16 @@ fun exclude( word: String){
     println( sentence.split('b'))
 
 }
+class Voulume(val pie: Double, val fraction:Double, var radius: Double){
+    fun volume(){
+        var total = pie*fraction*(radius*radius*radius)
+        println(total)
+    }
+}
 fun numbers(digit: Array<Int>){
     var operation = arrayOf(digit)
     println (operation.count())
+    println(operation.size())
 }
 fun volume(a: Double, pie:Double, r: Double){
     var sphere = a*pie*(r*r*r)
